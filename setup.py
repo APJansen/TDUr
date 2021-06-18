5e8461360ea1b15bd794e3839cddad7ff211f16b
Aron <aronpjansen@gmail.com>
2021-06-18 12:41:12 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='td_ur',
+    version='1.0',
+    packages=['td_ur'],
+    url='',
+    license='',
+    author='Aron Jansen',
+    author_email='',
+    description='AI agent playing the Royal game of Ur',
+    requires=['numpy', 'matplotlib', 'jax', 'ipywidgets']
+)
