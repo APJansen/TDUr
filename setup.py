f80aa3418bfafb11eb03680c2665f870a6c7ebf2
Aron <aronpjansen@gmail.com>
2021-07-04 10:21:13 +0200

    setup working

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ setup(
     name='td_ur',
     version='1.0',
     packages=['td_ur'],
-    url='',
+    package_data={'': ['*.pkl']},
+    include_package_data=True,
+    url='www.github.com/apjansen/TDUr',
     license='MIT',
     author='Aron Jansen',
     author_email='',
     description='AI agent playing the Royal game of Ur',
-    include_package_data=True,
     install_requires=['numpy', 'matplotlib', 'jax', 'jaxlib', 'ipywidgets']
 )
